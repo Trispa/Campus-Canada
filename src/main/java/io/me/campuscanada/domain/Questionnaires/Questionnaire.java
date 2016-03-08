2c0827a8b702196ae40f8c8e3b693b8300806877
Trispa <trispa88@gmail.com>
2016-03-07 20:59:57 -0500

    clean

--- src/main/java/io/me/campuscanada/domain/Questionnaires/Questionnaire.java
+++ src/main/java/io/me/campuscanada/domain/Questionnaires/Questionnaire.java
@@ -17,8 +17,8 @@ public class Questionnaire {
 	}
 	public Boolean isComplete(){
 		for(Question question :questionnairelist){
-			String answer = question.getAnswer();
-			if(answer == ""){
+		
+			if(question.getAnswer().isEmpty()){
 				if(question.isrequired())
 				return false;	
 			}
